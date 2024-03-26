945202c63fa74a7c1edb730ef319bbaca07e45fa
Lirae Que Data <liraedata59@gmail.com>
2024-03-26 22:58:19 +0800

    Add setup_project method implementation

--- main.py
+++ main.py
@@ -77,7 +77,50 @@ class AutoProjectStructure:
     
     
     def setup_project(self):
-        pass
+        contents = [
+            "from .base import *\n\n",
+            "DEBUG = False\n",
+            "ALLOWED_HOSTS = []\n",
+            "INTERNAL_IPS = [\"127.0.0.1\"]"
+        ]
+        
+        os.chdir("config")
+        os.mkdir("settings")
+        os.rename("settings.py", "settings/base.py")
+        os.chdir("settings")
+        self.create_structure("__init__.py")
+        self.create_structure("production.py", contents[:3])
+        contents[1] = "DEBUG = True\n"
+        self.create_structure("local.py", contents)
+        
+        with open("base.py", "r") as file:
+            lines = file.readlines()
+            file.close()
+        
+        lines = lines[:23] + lines[29:39] + ["\t\'rest_framework\',\n", "\t\'api\',\n"]  + lines[39:]
+
+        with open("base.py", "w") as file:
+            file.writelines(lines)
+            file.close()
+        
+        os.chdir("..")
+        
+        for file_name in ["asgi.py", "wsgi.py", "manage.py"]:
+            if file_name == "manage.py": 
+                os.chdir("..")
+                
+            with open(file_name, "r") as file:
+                lines = file.readlines()
+                file.close()
+                
+            index = 13 if file_name != "manage.py" else 8
+            lines[index] = lines[index][:-3] + ".local')\n"
+            
+            with open(file_name, "w") as file:
+                file.writelines(lines)
+                file.close()
+                
+        os.system("python manage.py migrate")
     
     
     def setup_version_control(self, github_repository):
